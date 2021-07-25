78183e65cdb3a14e04f5568efdc64168052c9319
Vicente Zapata <vzapata@ciisa.cl>
2021-07-24 21:58:15 -0400

    Update pipeline-main.groovy

--- pipeline-main.groovy
+++ pipeline-main.groovy
@@ -91,7 +91,7 @@ node {
         println "**************************************************************************************************" 
         //sh "rm ${WORKSPACE}/'INFORME DE VULNERABILIDADES - ${PROJECT}.pdf'"
         sh "java -jar /opt/scripts/api-sonarqube-report.jar admin ciisa2021 http://sonarqube:9000 /opt/scripts/OutputRapidscan.txt /opt/scripts/OutputTrivyJson.txt ${TYPEREPO} ${CATEGORY} ${GITPROJECT} ${PROJECT}"
-        sh "cp ${WORKSPACE}/'INFORME DE VULNERABILIDADES - ${PROJECT}.pdf'	/opt/scripts/"
+        //sh "cp ${WORKSPACE}/'INFORME DE VULNERABILIDADES - ${PROJECT}.pdf'	/opt/scripts/"
     }
     stage('Send Report') {
         println "**************************************************************************************************"
